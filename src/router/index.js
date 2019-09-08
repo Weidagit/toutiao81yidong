cd3a0951cb5bd94daee1a0cf1df3355b894d7ec1
Weidagit <1092724737@qq.com>
2019-09-08 19:47:17 +0800

    搜索-搭建界面

--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,11 @@ export default new Router({
         }
       ]
     },
-
+    {
+      path: '/search',
+      name: 'search',
+      component: () => import(/* webpackChunkName: "search" */ '../views/Search.vue')
+    },
     {
       path: '/login',
       name: 'login',
