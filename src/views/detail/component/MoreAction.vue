3abd0685dc6f1773f1b63eba6b0ed146c58490cb
Weidagit <1092724737@qq.com>
2019-09-10 16:53:23 +0800

    点赞和取消点赞不喜欢取消不喜欢登陆插件

--- src/views/detail/component/MoreAction.vue
+++ src/views/detail/component/MoreAction.vue
@@ -2,25 +2,25 @@
   <div class="more-action">
   <!-- 用户对文章的态度, -1: 无态度，0-不喜欢，1-点赞 -->
     <van-button
-       :icon="article.attitude === 1 ? 'star' : 'star-o' "
+      :icon="article.attitude === 1 ? 'good-job' : 'good-job-o' "
       round
       :loading="loading"
       type="danger"
-      @click="handleLike"
+      @click="handlepraise"
     >赞</van-button>
 
     <van-button
-      icon="delete"
+      :icon="article.attitude === 0 ? 'star-o' : 'star'"
       round
       :loading="false"
       type="danger"
-
+     @click="handlelike"
     >不喜欢</van-button>
   </div>
 </template>
 
 <script>
-import { likeArticle, unlikeArticle } from '../../../api/article'
+import { likeArticle, unlikeArticle, unDislikeArticle, dislikeArticle } from '../../../api/article'
 export default {
   props: ['article'],
   data () {
@@ -30,8 +30,11 @@ export default {
   },
   methods: {
     // 点赞和取消点赞
-    async handleLike () {
+    async handlepraise () {
       // 判断是否登录
+      if (!this.$checkLogin()) {
+        return
+      }
       // 点赞或者取消点赞
       try {
         // 用户对文章的态度, -1: 无态度，0-不喜欢，1-点赞
@@ -42,6 +45,24 @@ export default {
         } else {
           await likeArticle(this.article.art_id)
           this.article.attitude = 1
+        }
+      } catch (err) {
+        this.$toast.fail('操作失败')
+      }
+    },
+    // 喜欢和取消喜欢
+    async handlelike () {
+      // 判断是否登录
+      // 不喜欢或者取消不喜欢
+      try {
+        // 用户对文章的态度, -1: 无态度，0-不喜欢，1-点赞
+        if (this.article.attitude === 0) {
+          // 当前已经对文章不喜欢，取消不喜欢
+          await unDislikeArticle(this.article.art_id)
+          this.article.attitude = -1
+        } else {
+          await dislikeArticle(this.article.art_id)
+          this.article.attitude = 0
         }
       } catch (err) {
         this.$toast.fail('操作失败')
