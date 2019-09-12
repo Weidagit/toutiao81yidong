d0a83be3cd2eb03741897fa2e0eefe0dfd30414e
Weidagit <1092724737@qq.com>
2019-09-12 14:35:21 +0800

    封装获取关注/粉丝的api

--- src/api/user.js
+++ src/api/user.js
@@ -53,3 +53,29 @@ export const uploadPhoto = (key, file) => {
 export const getUserInfo = () => {
   return request.get('/app/v1_0/user')
 }
+
+// 获取关注用户列表
+export const getFollowings = ({
+  page,
+  perPpage
+}) => {
+  return request.get('/app/v1_0/user/followings', {
+    params: {
+      page,
+      per_page: perPpage
+    }
+  })
+}
+
+// 获取粉丝用户列表
+export const getFollowers = ({
+  page,
+  perPage
+}) => {
+  return request.get('/app/v1_0/user/followers', {
+    params: {
+      page,
+      per_page: perPage
+    }
+  })
+}
